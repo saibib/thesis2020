d903def2ef366d004ad6cd3a27ee839d9fb8e782
“saibib”​ <thesaimun@gmail.com>
2021-03-06 22:09:41 -0500

    gsmi eda

--- src/case_studies/SVI/SVI_eda.R
+++ src/case_studies/SVI/SVI_eda.R
@@ -1,6 +1,6 @@
-gsmi= read.csv(here('data','GSMI','GSMI_final.csv'))
 
-gsmi = gsmi %>% remove_rownames %>% column_to_rownames(var="X")
+colnames(ghi) =  c('Undernourishment', 'Child Wasting', 'Child Stunting', 'Child Mortality')
+
 library(corrplot)
 cor.mtest <- function(mat, ...) {
   mat <- as.matrix(mat)
@@ -16,26 +16,18 @@ cor.mtest <- function(mat, ...) {
   colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
   p.mat
 }
-p.mat = cor.mtest(gsmi)
+p.mat = cor.mtest(voi)
 
-firstup <- function(x) {
-  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
-  x
-}
-colnames(gsmi) = firstup(gsub("_", "\n", colnames(gsmi)))
-
-
-png(height=1100, width=1100, filename="figs/GSMI/gsmi_cor.png")
-cor(gsmi, use = 'pairwise') %>%
+png(height=800, width=800, filename="figs/svi/toi_cor.png")
+cor(voi, use = 'pairwise') %>%
   corrplot.mixed(lower = "number", upper = "ellipse",
                  tl.col = "black",
-                 p.mat = p.mat, sig.level = 0.01, insig = "blank",cl.cex = 2)
+                 p.mat = p.mat, sig.level = 0.01, insig = "blank")
 dev.off()
 
 
-colnames(gsmi) = gsub("\n", " ", colnames(gsmi))
 
-gsmi %>% as.data.frame() %>%
+ghi %>%
   tidyr::gather() %>%
   ggplot(aes(value)) +
   geom_histogram()+
@@ -44,32 +36,31 @@ gsmi %>% as.data.frame() %>%
   theme(axis.text.x = element_text(size = 15))+
   xlab('Value')+
   ylab('Count')+
-  ggtitle('Distribution of GSMI Dimension Scores')+
+  ggtitle('Distribution of GHI Dimension Scores')+
   theme_minimal()+
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=.5))+
-  ggsave('figs/GSMI/gsmi_eda_hist.png')
+  ggsave('figs/GHI/ghi_eda_hist.png')
 
 
-df = cbind(gsmi,gsmi_scores,gsmi_optim_scores)
-colnames(df) = firstup(gsub("_", " ", colnames(df)))
-colnames(df) = gsub(' ', '\n', colnames(df))
 
-png(height=1200, width=1200, filename="figs/GSMI/gsmi_scores_cor.png")
+df = cbind(ghi[intersect(rownames(ghi), names(ghi_scores)), ],ghi_scores,ghi_optim_scores)
+colnames(df) = c('Undernourishment', 'Child Wasting', 'Child Stunting', 'Child Mortality', 'Original Scores',
+                 'Optimized Scores')
+png(height=1000, width=1000, filename="figs/GHI/ghi_scores_cor.png")
 cor(df, use = 'pairwise') %>%
   corrplot.mixed(lower = "number", upper = "ellipse",
                  tl.col = "black",
-                 p.mat = cor.mtest(df), sig.level = 0.01, insig = "blank",cl.cex = 2)
+                 p.mat = cor.mtest(df, sig.level = 0.01, insig = "blank"))
 dev.off()
 
 
+|>|
+  # pairs(ghi)
+  # library(GGally)
+  # ggpairs(ghi, aes(alpha = 0.4),diag = list(discrete = 'barDiag'))
 
-# pairs(ghi)
-# library(GGally)
-# ggpairs(ghi, aes(alpha = 0.4),diag = list(discrete = 'barDiag'))
 
-
-#correlation matrix btw before and after scores
-library(naniar)
+  #correlation matrix btw before and after scores
+  library(naniar)
 library(visdat)
 
 miss_var_summary(ghi)
